5d05b95a49d647841dbf27a183e460f393f55199
isaacasher97 <isaac.asher97@gmail.com>
2023-07-01 18:22:33 -0400

    successfully added POST route for when creating a new animal

--- controllers/animal.js
+++ controllers/animal.js
@@ -5,11 +5,27 @@ const router = express.Router();
 
 router.get("/animals", async (req, res) => {
     const allAnimals = await Animal.find({})
-    res.render('animals/index.ejs', { animals: allAnimals})
+    res.render(
+        'animals/index.ejs',
+        { animals: allAnimals}
+    )
 })
 
 router.get("/new", (req, res) => {
     res.render('animals/new.ejs')
+})
+
+
+router.post('/animals', async (req, res) => {
+    if(req.body.extinct === 'on'){
+        req.body.extinct = true;
+    }else{
+        req.body.extinct = false;
+    }
+
+    await Animal.create(req.body);
+    res.redirect('/animals')
+    
 })
 
 module.exports = router;

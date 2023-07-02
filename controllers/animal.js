40a35df87db2dde291eb1478f59ed210b056a8a8
isaacasher97 <isaac.asher97@gmail.com>
2023-07-01 21:06:25 -0400

    CRUD functionality complete

--- controllers/animal.js
+++ controllers/animal.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Animal = require('../models/animal');
-
 const router = express.Router();
 
 router.get("/animals", async (req, res) => {
@@ -32,6 +31,27 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
     const animal = await Animal.findById(id);
     res.render('animals/show.ejs', {animal})
+}) 
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    await Animal.findByIdAndRemove(id);
+    res.redirect('/animals')
+})
+
+router.get('/:id/edit', async (req, res) => {
+    const id = req.params.id;
+
+    const animal = await Animal.findById(id);
+    res.render('animals/edit.ejs', {animal})
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    req.body.extinct = req.body.extinct === 'on' ? true : false;
+    await Animal.findByIdAndUpdate(id, req.body);
+    res.redirect('/animals')
 })
 
 module.exports = router;
